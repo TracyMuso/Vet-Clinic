6228a82d3d3b835abf95d143c87936541b6bfdad
Elfin Git <musongoletracy@gmail.com>
2022-10-25 13:29:36 +0200

    create table, insert data and make queries

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
+-- create database
+CREATE DATABASE vet_clinic;
+
 CREATE TABLE animals (
-    name varchar(100)
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(80),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL,
 );
